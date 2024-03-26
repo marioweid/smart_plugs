e28a17363436fc6eeaf34a079a5e44cbedb83b33
marioweid <mario.weidner@gmx.de>
2024-03-26 09:17:50 +0100

    fixed swagger example

--- backend/models/responses.py
+++ backend/models/responses.py
@@ -4,12 +4,12 @@ class DevicesResponse(BaseModel):
     db_id: int = Field(..., title="Database ID", description="The unique identifier of the device in the database")
     name: str = Field(..., title="Device Name", description="The name of the device")
     is_on: bool = Field(..., title="Device State", description="The current state of the device (on/off)")
-
-    class Config:
-        schema_extra = {
+    model_config = {
+        "json_schema_extra": {
             "example": {
                 "db_id": "7e5521fc-df5c-42d4-82e0-1e8fecf2485e",
                 "name": "Smart Plug",
                 "is_on": True
             }
         }
+    }
